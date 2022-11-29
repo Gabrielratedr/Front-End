d4b581eca60bfd7aa707ca9a936d816c6e34bd59
Gabrielratedr <gabrielsjobs@outlook.com>
2022-11-29 16:32:06 -0300

    Front-End

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './paginas/home/Home';
+import { Grid } from '@material-ui/core';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+
+
+
+function App() {
+  return (
+    <>
+    {/* <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+    </Grid> */}
+
+
+   <Router>
+    <Navbar />
+     <Routes>
+       <Route path='/' element={<Login />}/>
+       <Route path='/home' element={<Home />}/>
+       <Route path='/cadastrousuario' element={<CadastroUsuario />}/>
+       </Routes>
+    <Footer />
+     </Router>
+    </>
+  );
+}
+
+export default App;
